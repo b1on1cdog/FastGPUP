a2155af245ffbe1e2c37d290251cc13d2a69b70c
BlueDog <79947058+b1on1cdog@users.noreply.github.com>
2022-03-14 23:12:59 -0400

    Initial upload

---
+++ FastGPU-P/Form1.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace FastGPU_P
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gpuBox = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.vmBox = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.addButton = new System.Windows.Forms.Button();
+            this.AllocationLabel = new System.Windows.Forms.Label();
+            this.allocationBar = new MetroFramework.Controls.MetroTrackBar();
+            this.allocLabel = new System.Windows.Forms.Label();
+            this.installDriver = new System.Windows.Forms.Button();
+            this.RemoveButton = new System.Windows.Forms.Button();
+            this.mountVHD = new System.Windows.Forms.Button();
+            this.vhdBox = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // gpuBox
+            // 
+            this.gpuBox.FormattingEnabled = true;
+            this.gpuBox.Location = new System.Drawing.Point(25, 174);
+            this.gpuBox.Name = "gpuBox";
+            this.gpuBox.Size = new System.Drawing.Size(315, 28);
+            this.gpuBox.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 151);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "GPU";
+            // 
+            // vmBox
+            // 
+            this.vmBox.FormattingEnabled = true;
+            this.vmBox.Location = new System.Drawing.Point(23, 107);
+            this.vmBox.Name = "vmBox";
+            this.vmBox.Size = new System.Drawing.Size(317, 28);
+            this.vmBox.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(25, 81);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(31, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "VM";
+            // 
+            // addButton
+            // 
+            this.addButton.Location = new System.Drawing.Point(25, 301);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(144, 29);
+            this.addButton.TabIndex = 4;
+            this.addButton.Text = "Add";
+            this.addButton.UseVisualStyleBackColor = true;
+            this.addButton.Click += new System.EventHandler(this.addButton_Click);
+            // 
+            // AllocationLabel
+            // 
+            this.AllocationLabel.AutoSize = true;
+            this.AllocationLabel.Location = new System.Drawing.Point(27, 214);
+            this.AllocationLabel.Name = "AllocationLabel";
+            this.AllocationLabel.Size = new System.Drawing.Size(156, 20);
+            this.AllocationLabel.TabIndex = 6;
+            this.AllocationLabel.Text = "Allocation percentage";
+            // 
+            // allocationBar
+            // 
+            this.allocationBar.BackColor = System.Drawing.Color.Transparent;
+            this.allocationBar.Location = new System.Drawing.Point(27, 246);
+            this.allocationBar.Name = "allocationBar";
+            this.allocationBar.Size = new System.Drawing.Size(255, 29);
+            this.allocationBar.TabIndex = 7;
+            this.allocationBar.Text = "null";
+            this.allocationBar.Scroll += new System.Windows.Forms.ScrollEventHandler(this.allocationBar_Scroll);
+            // 
+            // allocLabel
+            // 
+            this.allocLabel.AutoSize = true;
+            this.allocLabel.Location = new System.Drawing.Point(297, 247);
+            this.allocLabel.Name = "allocLabel";
+            this.allocLabel.Size = new System.Drawing.Size(37, 20);
+            this.allocLabel.TabIndex = 8;
+            this.allocLabel.Text = "50%";
+            // 
+            // installDriver
+            // 
+            this.installDriver.Location = new System.Drawing.Point(91, 372);
+            this.installDriver.Name = "installDriver";
+            this.installDriver.Size = new System.Drawing.Size(243, 29);
+            this.installDriver.TabIndex = 9;
+            this.installDriver.Text = "install/update driver";
+            this.installDriver.UseVisualStyleBackColor = true;
+            this.installDriver.Click += new System.EventHandler(this.installDriver_Click);
+            // 
+            // RemoveButton
+            // 
+            this.RemoveButton.Location = new System.Drawing.Point(175, 301);
+            this.RemoveButton.Name = "RemoveButton";
+            this.RemoveButton.Size = new System.Drawing.Size(159, 29);
+            this.RemoveButton.TabIndex = 10;
+            this.RemoveButton.Text = "Remove";
+            this.RemoveButton.UseVisualStyleBackColor = true;
+            this.RemoveButton.Click += new System.EventHandler(this.RemoveButton_Click);
+            // 
+            // mountVHD
+            // 
+            this.mountVHD.Location = new System.Drawing.Point(27, 338);
+            this.mountVHD.Name = "mountVHD";
+            this.mountVHD.Size = new System.Drawing.Size(307, 29);
+            this.mountVHD.TabIndex = 11;
+            this.mountVHD.Text = "Mount VHD";
+            this.mountVHD.UseVisualStyleBackColor = true;
+            this.mountVHD.Click += new System.EventHandler(this.mountVHD_Click);
+            // 
+            // vhdBox
+            // 
+            this.vhdBox.FormattingEnabled = true;
+            this.vhdBox.Location = new System.Drawing.Point(27, 373);
+            this.vhdBox.Name = "vhdBox";
+            this.vhdBox.Size = new System.Drawing.Size(58, 28);
+            this.vhdBox.TabIndex = 12;
+            this.vhdBox.Click += new System.EventHandler(this.vhdBox_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(91, 429);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(176, 20);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "with ❤ by @b1on1cdog";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(360, 465);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.vhdBox);
+            this.Controls.Add(this.mountVHD);
+            this.Controls.Add(this.RemoveButton);
+            this.Controls.Add(this.installDriver);
+            this.Controls.Add(this.allocLabel);
+            this.Controls.Add(this.allocationBar);
+            this.Controls.Add(this.AllocationLabel);
+            this.Controls.Add(this.addButton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.vmBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.gpuBox);
+            this.Name = "Form1";
+            this.Text = "Fast GPU-P";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private ComboBox gpuBox;
+        private Label label1;
+        private ComboBox vmBox;
+        private Label label2;
+        private Button addButton;
+        private Label AllocationLabel;
+        private MetroFramework.Controls.MetroTrackBar allocationBar;
+        private Label allocLabel;
+        private Button installDriver;
+        private Button RemoveButton;
+        private Button mountVHD;
+        private ComboBox vhdBox;
+        private Label label3;
+    }
+}
---
+++ FastGPU-P/Form1.cs
@@ -0,0 +1,307 @@
+using MetroFramework.Forms;
+using Cudafy;
+using Cudafy.Host;
+using System.Management.Automation;
+using System.Diagnostics;
+using System.Collections.ObjectModel;
+
+
+namespace FastGPU_P
+{
+    public partial class Form1 : MetroForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Shown += Form1_Shown;
+        }
+
+        private void Form1_Shown(Object sender, EventArgs e)
+        {
+            int i = 0;
+            foreach (GPGPUProperties prop in CudafyHost.GetDeviceProperties(CudafyModes.Target, false))
+            {
+                gpuBox.Items.Add(prop.Name);
+                Console.WriteLine();
+
+                i++;
+            }
+
+            string _scr = ("Get-VM | Select -ExpandProperty Name");
+            var _ps = PowerShell.Create();
+            _ps.AddScript(_scr);
+            Collection<PSObject> _cObj = _ps.Invoke();
+
+            if (_ps.HadErrors)
+            {
+                Console.WriteLine(_ps.Streams.Error[0].ToString());
+            }
+
+            foreach (PSObject _vm in _cObj)
+            {
+                vmBox.Items.Add(_vm);
+            }
+
+        }
+
+        private void allocationBar_Scroll(object sender, ScrollEventArgs e)
+        {
+            this.allocLabel.Text = allocationBar.Value.ToString()+"%";
+        }
+        private string getGPUInstance(string name)
+        {
+            string _scr = (@"
+        $PartitionableGPUList = Get-CimInstance -Class Msvm_PartitionableGpu -Namespace root\virtualization\v2 
+        $DeviceID = ((Get-CimInstance -ClassName Win32_PNPSignedDriver | where {($_.Devicename -eq " + "\""+name+"\""+ @")}).hardwareid).split('\')[1]
+        $DevicePathName = ($PartitionableGPUList | Where-Object name -like *$DeviceID*).Name
+        $DevicePathName | Select
+");
+            var _ps = PowerShell.Create();
+            _ps.AddScript(_scr);
+            Collection<PSObject> _cObj = _ps.Invoke();
+            foreach (PSObject _id in _cObj)
+            {
+                return _id.ToString();
+            }
+
+            if (_ps.HadErrors)
+            {
+                MessageBox.Show(_ps.Streams.Error[0].ToString());
+            }
+            throw new Exception("instance id not found");
+        }
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            string _scr = (@"
+    Set-ExecutionPolicy -ExecutionPolicy Unrestricted
+    $VMName = " + "\""+ vmBox.GetItemText(vmBox.Text) + "\"" +@"
+    $instance = "+ "\""+ getGPUInstance(gpuBox.Text) + "\""+ @"
+    [decimal]$GPUResourceAllocationPercentage = " + allocationBar.Value.ToString() + @"
+    Add-VMGpuPartitionAdapter -VMName $VMName -InstancePath $instance
+    [float]$devider = [math]::round($(100 / $GPUResourceAllocationPercentage),2)
+    Set-VMGpuPartitionAdapter -VMName $VMName -MinPartitionVRAM ([math]::round($(1000000000 / $devider))) -MaxPartitionVRAM ([math]::round($(1000000000 / $devider))) -OptimalPartitionVRAM ([math]::round($(1000000000 / $devider)))
+    Set-VMGPUPartitionAdapter -VMName $VMName -MinPartitionEncode ([math]::round($(18446744073709551615 / $devider))) -MaxPartitionEncode ([math]::round($(18446744073709551615 / $devider))) -OptimalPartitionEncode ([math]::round($(18446744073709551615 / $devider)))
+    Set-VMGpuPartitionAdapter -VMName $VMName -MinPartitionDecode ([math]::round($(1000000000 / $devider))) -MaxPartitionDecode ([math]::round($(1000000000 / $devider))) -OptimalPartitionDecode ([math]::round($(1000000000 / $devider)))
+    Set-VMGpuPartitionAdapter -VMName $VMName -MinPartitionCompute ([math]::round($(1000000000 / $devider))) -MaxPartitionCompute ([math]::round($(1000000000 / $devider))) -OptimalPartitionCompute ([math]::round($(1000000000 / $devider)))
+    Set-VM -GuestControlledCacheTypes $true -VMName $VMName
+    Set-VM -LowMemoryMappedIoSpace 1Gb -VMName $VMName
+    Set-VM -HighMemoryMappedIoSpace 32GB -VMName $VMName
+");
+            var _ps = PowerShell.Create();
+            _ps.AddScript(_scr);
+            Collection<PSObject> _cObj = _ps.Invoke();
+            if (_ps.HadErrors)
+            {
+                MessageBox.Show(_ps.Streams.Error[0].ToString());
+            }
+            else
+            {
+                MessageBox.Show("GPU Added to VM successfully!");
+            }
+        }
+
+        private void installDriver_Click(object sender, EventArgs e)
+        {
+            //
+            string _scr = @"
+Set-ExecutionPolicy -ExecutionPolicy Unrestricted
+Import-Module Storage
+Function Add-VMGpuPartitionAdapterFiles {
+param(
+[string]$hostname = $ENV:COMPUTERNAME,
+[string]$DriveLetter,
+[string]$GPUName
+)
+
+If (!($DriveLetter -like ""*:*"")) {
+    $DriveLetter = $Driveletter + "":""
+    }
+
+If ($GPUName -eq ""AUTO"") {
+    $PartitionableGPUList = Get-CimInstance -Class Msvm_PartitionableGpu -Namespace root\virtualization\v2 
+    $DevicePathName = $PartitionableGPUList.Name | Select-Object -First 1
+    $GPU = Get-PnpDevice | Where-Object {($_.DeviceID -like ""*$($DevicePathName.Substring(8,16))*"") -and ($_.Status -eq ""OK"")} | Select-Object -First 1
+    $GPUName = $GPU.Friendlyname
+    $GPUServiceName = $GPU.Service 
+    }
+Else {
+    $GPU = Get-PnpDevice | Where-Object {($_.Name -eq ""$GPUName"") -and ($_.Status -eq ""OK"")} | Select-Object -First 1
+    $GPUServiceName = $GPU.Service
+    }
+# Get Third Party drivers used, that are not provided by Microsoft and presumably included in the OS
+
+Write-Host ""INFO   : Finding and copying driver files for $GPUName to VM. This could take a while...""
+
+$Drivers = Get-CimInstance -ClassName Win32_PNPSignedDriver | where {$_.DeviceName -eq ""$GPUName""}
+
+New-Item -ItemType Directory -Path ""$DriveLetter\windows\system32\HostDriverStore"" -Force | Out-Null
+#copy directory associated with sys file 
+$servicePath = (Get-CimInstance -ClassName Win32_SystemDriver | Where-Object {$_.Name -eq ""$GPUServiceName""}).Pathname
+                $ServiceDriverDir = $servicepath.split('\')[0..5] -join('\')
+                $ServicedriverDest = (""$driveletter"" + ""\"" + $($servicepath.split('\')[1..5] -join('\'))).Replace(""DriverStore"",""HostDriverStore"")
+                if (!(Test-Path $ServicedriverDest)) {
+                Copy-item -path ""$ServiceDriverDir"" -Destination ""$ServicedriverDest"" -Recurse
+                }
+
+# Initialize the list of detected driver packages as an array
+$DriverFolders = @()
+foreach ($d in $drivers) {
+    $DriverFiles = @()
+    $ModifiedDeviceID = $d.DeviceID -replace ""\\"", ""\\""
+    $Antecedent = ""\\"" + $hostname + ""\ROOT\cimv2:Win32_PNPSignedDriver.DeviceID=""""$ModifiedDeviceID""""""
+    $DriverFiles += Get-CimInstance -ClassName Win32_PNPSignedDriverCIMDataFile | where {$_.Antecedent -eq $Antecedent}
+    $DriverName = $d.DeviceName
+    $DriverID = $d.DeviceID
+    if ($DriverName -like ""NVIDIA*"") {
+        New-Item -ItemType Directory -Path ""$driveletter\Windows\System32\drivers\Nvidia Corporation\"" -Force | Out-Null
+        }
+    foreach ($i in $DriverFiles) {
+            $path = $i.Dependent.Split(""="")[1] -replace '\\\\', '\'
+            $path2 = $path.Substring(1,$path.Length-2)
+            $InfItem = Get-Item -Path $path2
+            $Version = $InfItem.VersionInfo.FileVersion
+            If ($path2 -like ""c:\windows\system32\driverstore\*"") {
+                $DriverDir = $path2.split('\')[0..5] -join('\')
+                $driverDest = (""$driveletter"" + ""\"" + $($path2.split('\')[1..5] -join('\'))).Replace(""driverstore"",""HostDriverStore"")
+                if (!(Test-Path $driverDest)) {
+                Copy-item -path ""$DriverDir"" -Destination ""$driverDest"" -Recurse
+                }
+            }
+            Else {
+                $ParseDestination = $path2.Replace(""c:"", ""$driveletter"")
+                $Destination = $ParseDestination.Substring(0, $ParseDestination.LastIndexOf('\'))
+                if (!$(Test-Path -Path $Destination)) {
+                    New-Item -ItemType Directory -Path $Destination -Force | Out-Null
+                    }
+                Copy-Item $path2 -Destination $Destination -Force
+                
+            }
+
+    }
+    }
+
+}
+[string]$VMName = " + "\"" + vmBox.Text + "\"" + @"
+[string]$GPUName = " + "\"" + gpuBox.Text + "\"" + @"
+[string]$Hostname = $ENV:Computername
+$VM = Get-VM -VMName $VMName
+$VHD = Get-VHD -VMId $VM.VMId
+
+If ($VM.state -eq ""Running"") {
+    [bool]$state_was_running = $true
+    }
+
+if ($VM.state -ne ""Off""){
+    ""Attemping to shutdown VM...""
+    Stop-VM -Name $VMName -Force
+    } 
+
+While ($VM.State -ne ""Off"") {
+    Start-Sleep -s 3
+    ""Waiting for VM to shutdown - make sure there are no unsaved documents...""
+    }
+
+""Mounting Drive...""
+$DriveLetter = "  + "\"" + vhdBox.Text+"\""+@"
+
+""Copying GPU Files - this could take a while...""
+Add-VMGPUPartitionAdapterFiles -hostname $Hostname -DriveLetter $DriveLetter -GPUName $GPUName
+
+""Dismounting Drive...""
+Dismount-VHD -Path $VHD.Path
+
+If ($state_was_running){
+    ""Previous State was running so starting VM...""
+    Start-VM $VMName
+    }
+
+";
+            var _ps = PowerShell.Create();
+            _ps.AddScript(_scr);
+            Collection<PSObject> _cObj = _ps.Invoke();
+            if (_ps.HadErrors)
+            {
+                MessageBox.Show(_ps.Streams.Error[0].ToString());
+            }
+            else
+            {
+                MessageBox.Show("GPU driver updated successfully!");
+            }
+            //
+        }
+        private void LoadDrives()
+        {
+
+            vhdBox.Items.Clear();
+            string _sdr = (@"(Get-PSDrive -PSProvider FileSystem).Root");
+            var _pz = PowerShell.Create();
+            _pz.AddScript(_sdr);
+            Collection<PSObject> _zObj = _pz.Invoke();
+            if (_pz.HadErrors)
+            {
+                MessageBox.Show(_pz.Streams.Error[0].ToString());
+            }
+            else
+            {
+                foreach (PSObject _driveletter in _zObj)
+                {
+                    string letter = _driveletter.ToString();
+                    letter = letter.Replace(":\\", "");
+                    if (letter != "C" && letter.Length < 3)
+                    {
+                        vhdBox.Items.Add(letter);
+                    }
+
+                }
+            }
+        }
+        private void mountVHD_Click(object sender, EventArgs e)
+        {
+            string _scr = (@"
+Import-Module Storage
+Set-ExecutionPolicy -ExecutionPolicy Unrestricted
+[string]$VMName = " + "\"" + vmBox.Text + "\"" + @"
+$VM = Get-VM -VMName $VMName
+$VHD = Get-VHD -VMId $VM.VMId
+Mount-VHD -Path $VHD.Path -PassThru
+");
+            var _ps = PowerShell.Create();
+            _ps.AddScript(_scr);
+            Collection<PSObject> _cObj = _ps.Invoke();
+            if (_ps.HadErrors)
+            {
+                MessageBox.Show(_ps.Streams.Error[0].ToString());
+            }
+            else
+            {
+                Process procDM = new Process();
+                procDM.StartInfo.FileName = "C:\\Windows\\System32\\mmc.exe";
+                procDM.StartInfo.Arguments = "C:\\Windows\\System32\\diskmgmt.msc";
+                procDM.Start();
+                MessageBox.Show("VHD Mounted Successfully!");
+                LoadDrives();
+            }
+        }
+
+        private void vhdBox_Click(object sender, EventArgs e)
+        {
+            LoadDrives();
+        }
+
+        private void RemoveButton_Click(object sender, EventArgs e)
+        {
+            string _scr = ("Remove-VMGpuPartitionAdapter -VMName " +"\"" + vmBox.Text + "\"");
+            var _ps = PowerShell.Create();
+            _ps.AddScript(_scr);
+            _ps.Invoke();
+            if (_ps.HadErrors)
+            {
+                Console.WriteLine(_ps.Streams.Error[0].ToString());
+            }
+            else {
+                MessageBox.Show("successfully removed all GPU Partitions from VM");
+            }
+        }
+    }
+}
+
